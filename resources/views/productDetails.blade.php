e2a6942c1acc33bcfaf36ed2b3aac59c16d8adee
Lê Thịnh <20211tt0718@mail.tdc.edu.vn>
2022-12-10 17:01:23 +0700

    yeu cau 5

--- resources/views/productDetails.blade.php
+++ resources/views/productDetails.blade.php
@@ -41,7 +41,7 @@
                     </div>
                 </div>
                 <div class="text-center py-4">
-                    <a class="h6 text-decoration-none text-truncate" href="">{{$item->product->name}} @foreach($item->item_configuration as $item_config) ({{$item_config->variation->name}}:{{$item_config->value}}) @endforeach</a>
+                    <a class="h6 text-decoration-none " href=""><p style="word-break: break-all">{{$item->product->name}} @foreach($item->item_configuration as $item_config) ({{$item_config->variation->name}}:{{$item_config->value}}) @endforeach</p></a>
                     <div class="d-flex align-items-center justify-content-center mt-2">
                         <h5>${{$item->sale_price}}</h5>
                         <h6 class="text-muted ml-2"><del>${{$item->price}}</del></h6>
